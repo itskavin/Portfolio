b9d84dd97450cd572dfe2e9c2632e96de35b96c3
T Kavin <91929982+Kavinthangavel@users.noreply.github.com>
2024-01-30 18:53:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text",{
-    strings: ["A Coder","A Youtuber","An Editor"],
+    strings: ["A Coder","A Youtuber"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
